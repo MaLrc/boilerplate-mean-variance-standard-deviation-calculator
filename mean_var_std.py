e4095f5d612361a2658c4f2e2dd15db369b96ead
Maël Larcher <ma.larcher.aag@gmail.com>
2025-02-17 20:01:05 +0000

    complete challenge

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,7 +2,20 @@ import numpy as np
 
 def calculate(list):
 
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.") 
+    else :
+        matrix = np.array(list).reshape(3, 3)
 
+        calculations = {
+            'mean': [matrix.mean(axis=0).tolist(), matrix.mean(axis=1).tolist(), matrix.flatten().mean()],
+            'variance': [matrix.var(axis=0).tolist(), matrix.var(axis=1).tolist(), matrix.flatten().var()],
+            'standard deviation': [matrix.std(axis=0).tolist(), matrix.std(axis=1).tolist(), matrix.flatten().std()],
+            'max': [matrix.max(axis=0).tolist(), matrix.max(axis=1).tolist(), matrix.flatten().max()],
+            'min': [matrix.min(axis=0).tolist(), matrix.min(axis=1).tolist(), matrix.flatten().min()],
+            'sum': [matrix.sum(axis=0).tolist(), matrix.sum(axis=1).tolist(), matrix.flatten().sum()]
+        }
 
+        return calculations
 
-    return calculations
+print(calculate([2,6,2,8,4,0,1,5,7]))
